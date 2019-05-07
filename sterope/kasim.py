51db2bb4a9aea34c0f95ec5eb4b367e586fefeab
glucksfall <glucksfall@users.noreply.github.com>
2019-05-07 14:52:48 -0400

    extra config to SLURM

--- sterope/kasim.py
+++ sterope/kasim.py
@@ -88,7 +88,7 @@ def argsparser():
 
 	# distribute computation with SLURM, otherwise with python multiprocessing API
 	parser.add_argument('--slurm'  , metavar = 'str'  , type = str  , required = False, default = None            , help = 'SLURM partition to use, default None')
-	parser.add_argument('--sbtach' , metavar = 'str'  , type = str  , required = False, default = ''              , help = 'explicit configuration for sbatch, e.g. --mem-per-cpu 5G')
+	parser.add_argument('--sbatch' , metavar = 'str'  , type = str  , required = False, default = ''              , help = 'explicit configuration for sbatch, e.g. --mem-per-cpu 5G')
 
 	# general options
 	parser.add_argument('--type'   , metavar = 'str'  , type = str  , required = False, default = 'global'        , help = 'global or local sensitivity analysis')
@@ -129,7 +129,7 @@ def ga_opts():
 		#'nfsim'     : os.path.expanduser(args.nfsim), # nfsim only
 		'python'    : os.path.expanduser(args.python),
 		'slurm'     : args.slurm,
-		'others'    : args.sbtach,
+		'others'    : args.sbatch,
 		'p_levels'  : int(args.grid),
 		'type'      : args.type,
 		'size'      : args.size,
