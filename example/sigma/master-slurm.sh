fce6d380b313bfdab9e29935da782b62ff44a606
glucksfall <glucksfall@users.noreply.github.com>
2019-05-06 23:58:52 -0400

    added example

--- example/sigma/master-slurm.sh
+++ example/sigma/master-slurm.sh
@@ -13,12 +13,12 @@
 
 export PYTHONPATH="$PYTHONPATH:$HOME/opt/github-repositories/sterope.glucksfall"
 
-MODEL=pysbmodel-example6-kasim.kappa
-FINAL=660
+MODEL=model.kappa
+FINAL=90
 STEPS=10
 
-NUM_LEVELS=1
+NUM_LEVELS=1000
 
 python3 -m sterope.kasim --model=$MODEL --final=$FINAL --steps=$STEPS \
---grid=$NUM_LEVELS --tmin=600 --type global --syntax=3 \
---kasim=kasim4-rc1 --python /usr/bin/python3 --slurm=$SLURM_JOB_PARTITION
+--grid=$NUM_LEVELS --type global --syntax=3 \
+--kasim=kasim4 --python /usr/bin/python3
