d27ccecb6b31999e1e30c6bb9c47c608d26c2fdc
Rodrigo <glucksfall@users.noreply.github.com>
2019-09-12 17:40:52 -0300

    Dask for simulation

--- sterope/kasim.py
+++ sterope/kasim.py
@@ -274,10 +274,9 @@ def simulate():
 			model_name = population[model[0], 'model']
 			output = 'model_{:s}.out.txt'.format(model_name)
 
-			job_desc['exec_kasim'] = '{:s} -i model_{:s}.kappa -l {:s} -p {:s} -o {:s} -syntax {:s} --no-log' \
+			cmd = '{:s} -i model_{:s}.kappa -l {:s} -p {:s} -o {:s} -syntax {:s} --no-log' \
 				.format(opts['kasim'], model_name, opts['final'], opts['steps'], output, opts['syntax'])
-
-			cmd = os.path.expanduser(job_desc['exec_kasim'])
+			cmd = os.path.expanduser(cmd)
 			cmd = re.findall(r'(?:[^\s,"]|"+(?:=|\\.|[^"])*"+)+', cmd)
 			squeue.append(cmd)
 
